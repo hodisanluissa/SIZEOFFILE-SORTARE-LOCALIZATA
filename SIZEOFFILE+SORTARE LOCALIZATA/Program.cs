3a5ef87af41d896a3f5ab93d804823f7062f284e
hodisanluissa <luissa_izabela@yahoo.com>
2020-04-10 15:19:41 +0300

    Add project files.

---
+++ SIZEOFFILE+SORTARE LOCALIZATA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SIZEOFFILE_SORTARE_LOCALIZATA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            //string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"..\..\input.txt");
+            //string vocale = "aeiouAEIOU";
+            //while ((line = file.ReadLine()) != null)
+
+            const string filename = @"../../input.txt";
+            FileInfo info = new FileInfo(filename);
+            long lenght = info.Length;
+            Console.WriteLine($"size file :{lenght}");
+
+            file.Close();
+             
+        }
+    }
+}
